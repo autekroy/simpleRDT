79ac0bf55298151fc3aca61c79ea5cae2dd6eb15
autekroy <autek.roy@gmail.com>
2015-11-22 01:46:48 -0800

    basic receiver structure

--- receiver.c
+++ receiver.c
@@ -13,33 +13,57 @@
 
 void error(char *msg)
 {
-    perror(msg);
+    fprintf(stderr, "%s", msg);
     exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd;
-    struct sockaddr_in snd_addr;
+    int sockfd, portno;
+    struct sockaddr_in serv_addr;
+    struct hostent *server; //contains tons of information, including the server's IP address
+    char *hostname, *filename;
 
 
-    if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }
+    if (argc != 4)
+        error("Usage: ./receiver <sender hostname> <sender portnumber> <filenam>\n");
+
+    hostname = argv[1];
+    portno = atoi(argv[2]);// port number
+    filename = argv[3];
+    // printf("%s %d %s\n", hostname, portno, filename);
 
     // SOCK_DGRAM for UDP transfer
     // AF_INET force IPv4
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);   //create socket
-    if (sockfd < 0) error("ERROR opening socket");
-    memset((char *) &snd_addr, 0, sizeof(serv_addr));  //reset memory
+    if (sockfd < 0) 
+        error("ERROR opening socket");
 
-    //fill in address info for sender
-    snd_addr.sin_family = AF_INET;
-    snd_addr.sin_addr.s_addr = htonl(atoi(argv[1]));;
-    snd_addr.sin_port = htons(atoi(argv[2]));
+    memset((char *) &serv_addr, 0, sizeof(serv_addr));  //reset memory
+    serv_addr.sin_family = AF_INET; //initialize server's address
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
 
+    // socket doesn't need to bind(), we just let OS assign a port number automatically
+
+    server = gethostbyname(hostname);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
     
+    // Build a request packet
+    printf("Building request packet for file: %s\n", filename);
+
+    // Send request packet
+
+    // get Ack for this request
+
+    // check if file exist
+
+    // receiving files
+
+    // maybe send FIN flag packet
      
     return 0; 
 }
