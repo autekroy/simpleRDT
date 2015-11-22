bd4514d8925b8660d9d205aed85c47b3bb00b547
Lui Liu <liulewie@gmail.com>
2015-11-22 15:41:39 -0800

    merge

--- packet.h
+++ packet.h
@@ -1,5 +1,6 @@
-#define PACKET_SIZE 1024
-#define TIMEOUT 10
+#define PACKET_SIZE 	1024
+#define TIMEOUT			500		//in miliseconds 		
+//#define MAXWINDOWSIZE 	10
 
 int base;
 int nextseqnum;
@@ -9,7 +10,7 @@ typedef enum packet_type{ACK, DATA}packet_type_t
 typedef struct packet {
   packet_type_t type;// 1 byte ACK/DATA
   int seqnum;	// sequence number: 4 byte
-  int size;	// file size: 4 byte
+  int size;	// total file size: 4 byte
   char data[PACKET_SIZE];
 }packet_t;
 
@@ -40,17 +41,23 @@ char* build_packet()
 
 
 
-int rdt_send(char* buffer, int length, int sockfd, struct sockaddr_in addr)
+int rdt_send(char* buffer, int length, int sockfd, struct sockaddr_in addr, int cwnd)
 {
 
 	signal(SIGALRM, alarm_handler);
 	/*
 
-	initialize
+	initialize variables
+	totalpacket = 
+
+	int base = 1 //starting sequence number from 1
+
 	send initial window of packets
 
+	receive initial acknowledgement
 
-	while (base < length)
+
+	while (base < totalpacket)
 	{
 		if(select (int maxfdp, fd_set *readset, fd_set *writeset, fd_set*excepset, TIMEOUT))
 		//returns the number of socket descriptor 
@@ -63,7 +70,8 @@ int rdt_send(char* buffer, int length, int sockfd, struct sockaddr_in addr)
 		            setitimer(0)
 				else 
 					setitimer(TIMEOUT)
-			
+			//add print statement
+
 			//send data
 			while there are remaining data in the window
 				if(nextseqnum == base) //when sending first packet
@@ -71,31 +79,44 @@ int rdt_send(char* buffer, int length, int sockfd, struct sockaddr_in addr)
 				build_data(DATA, buffer) //set end_flag header as necessary
 				udt_send(data)
 				nextseqnum++
-	}
+			//add print statement
+	}		
+	return 0;
 	*/
+
 
 }
 
 
-
-
+//return pointer to 
+char* rdt_receive(int* length, int sockfd, struct sockaddr_in addr)
+{
 /*
-//receiver
-rdt_receive()
-char* buffer;
-int expectedseqnum;
-while(1){
-select (int maxfdp, fd_set *readset, fd_set *writeset, fd_set*excepset, TIMEOUT)
-process_data (int& ending_flag, int& seqnum)
-if (ending_flag == 1) break;
 
-if (seqnum == expectedseqnum)
-append_data(data)
-build_data(ACK, buffer) 
-udt_send(ACK, expectedseqnum)
-expectedseqnum++
-else 
-	build_data(ACK, buffer) 
-udt_send(ACK, expectedseqnum-1)
+	receive the first packet that declares the number of packets (blocking)
+	send ACK
+
+	initialize variables
+	int expectedseqnum = 1;
+
+	while(expectedseqnum < totalpacket){
+		select (int maxfdp, fd_set *readset, fd_set *writeset, fd_set*excepset, TIMEOUT)
+		{
+			
+			process_data (int& seqnum)
+			if (seqnum == 1)
+				initialization work
+				char* buffer and dynamically allocate buffer
+
+			if (seqnum == expectedseqnum)
+				append_data(data)
+				build_data(ACK, expectedseqnum) 
+				udt_send(ACK, expectedseqnum)
+				expectedseqnum++
+			else 
+				build_data(ACK, buffer) 
+				udt_send(ACK, expectedseqnum-1)
+		}
+	}
 }
 */
